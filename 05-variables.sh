940e2b8707b0ef5316141233cba562781ad4de48
hareesh5jampala <hareesh5jampala.com>
2024-09-02 19:06:29 +0530

    variables

---
+++ 05-variables.sh
@@ -0,0 +1,13 @@
+#!/bin/bash/
+
+echo "Please enter your username::"
+
+read -s USERNAME #takes input USERNAME variable
+
+echo "Username entered is:$USERNAME"
+
+echo "Please enter your password::"
+
+read -s PASSWORD
+
+echo "Username eneter is:$USERNAME"
